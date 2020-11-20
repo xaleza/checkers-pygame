4c835fdf510a4eef11f4fc1e2b55083aa0ce947e
xaleza <goncalo.almeida.8@hotmail.com>
2020-11-20 13:00:06 +0000

    Board and pieces drawn

---
+++ checkers/board.py
@@ -0,0 +1,40 @@
+import pygame
+from .constants import BLACK, WHITE, BROWN, YELLOW, ROWS, COLS, SQUARE_SIZE
+from .piece import Piece
+
+class Board:
+    def __init__(self):
+        self.board = []
+        self.selected_piece = None
+        self.black_left = self.white_left = 12
+        self.black_kings = self.white_kings = 0
+        self.create_board()
+ 
+    def draw_squares(self, win):
+        win.fill(BROWN)
+        for row in range(ROWS):
+            for cols in range(row % 2, COLS, 2):
+                pygame.draw.rect(win, YELLOW, (row*SQUARE_SIZE, cols*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+
+    def create_board(self):
+        for row in range(ROWS):
+            self.board.append([])
+            for col in range(COLS):
+                if col % 2 == ((row + 1) %2):
+                    if row < 3:
+                        self.board[row].append(Piece(row, col, WHITE))
+                    elif row > 4:
+                        self.board[row].append(Piece(row,col,BLACK))
+                    else:
+                        self.board[row].append(0)
+                else:
+                    self.board[row].append(0)
+    
+    def draw(self,win):
+        self.draw_squares(win)
+        for row in range(ROWS):
+            for col in range(COLS):
+                piece = self.board[row][col]
+                if piece != 0:
+                    piece.draw(win)
+
